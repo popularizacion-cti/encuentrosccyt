df85647ad35e15fcb14b6265e84a3cf964e3e538
popularizacion-cti <popularizacion@concytec.gob.pe>
2026-06-09 16:18:55 -0500

    Create config-tailwind.js

---
+++ config-tailwind.js
@@ -0,0 +1,15 @@
+// Definición de la paleta corporativa y extensiones para el CDN
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                brand: '#F79131',   // Naranja corporativo
+                dark: '#7A2C8E',    // Morado principal
+                accent: '#4DB748',  // Verde de realce
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            }
+        }
+    }
+}
